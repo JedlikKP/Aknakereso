4bac6a09986264439c4fd92a40612b458f39cb22
kenyeres.patrik <kenyeres.patrik@C41-14>
2020-02-04 13:39:22 +0100

    új dolog

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,10 +8,16 @@ namespace ConsoleApp1
 {
     class Program
     {
+        char[,] pálya = new char[10, 10];
         static void Main(string[] args)
         {
-            Console.WriteLine("CSÁ");
+            Feltöltés();
             Console.ReadKey();
+        }
+
+        private static void Feltöltés()
+        {
+            
         }
     }
 }
